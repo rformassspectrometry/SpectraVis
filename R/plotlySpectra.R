a93f5c05f641937c13b238597fe52ebc2902763a
jorainer <johannes.rainer@gmail.com>
2022-03-30 08:46:26 +0200

    refactor: refactor plotlySpectra
    - `plotlySpectra` creates an empty plot for an empty `Spectra` instead of
      throwing an error.

--- R/plotlySpectra.R
+++ R/plotlySpectra.R
@@ -11,19 +11,40 @@
 ##'
 ##' @return A `plotly` object.
 ##'
-##' @importFrom plotly plot_ly add_segments
+##' @importFrom plotly plot_ly add_segments layout
 ##'
 ##' @export
 ##'
-##' @author Laurent Gatto
+##' @author Laurent Gatto, Johannes Rainer
 plotlySpectra <- function(object) {
     stopifnot(inherits(object, "Spectra"))
+    p <- plot_ly()
+    p <- layout(p, xaxis = list(title = "m/z"),
+                yaxis = list(title = "intensity", zeroline = TRUE),
+                hovermode = "x", hoverdistance = 1)
+    if (length(object) == 0) return(p)
     if (length(object) != 1)
         stop("Object must be of length 1.")
     x <- data.frame(peaksData(object)[[1]])
     x$zero <- 0
-    p <- plot_ly(data = x,
-            x = ~mz, y= ~zero,
-            xend = ~mz, yend = ~intensity)
-    add_segments(p)
+    .plotly_peaks(p, x)
+}
+
+#' @param p `plot_ly` instance.
+#'
+#' @param data `data.frame` with the data to be plotted.
+#'
+#' @param col colors for the segments. Can be of length 1 or equal to the number
+#'     of peaks.
+#'
+#' @param name `character(1)` defining the name of the spectrum.
+#'
+#' @author Johannes Rainer
+#' 
+#' @noRd
+.plotly_peaks <- function(p, data, col = "#525252", name = "") {
+    add_segments(p, data = data, x = ~mz, y = ~zero, xend = ~mz,
+                 yend = ~intensity, colors = col, color = col, 
+                 name = name,
+                 hovertemplate = "<br>mz: %{x}<br>int: %{y}<br>")
 }
